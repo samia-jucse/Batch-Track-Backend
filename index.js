86c9e55507439611115f43cc7c2224389d5f03c4
Toufiklabib <jucse389@gmail.com>
2024-10-29 17:11:08 +0600

    Last Update

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ app.get("/", (req, res) => {
     res.send("hello world!");
 });
 
-/// Here Every one add their controller and ensure import/require top op the project
-/// This is my Route/Controller
+
 app.use("/api/batch-validate",validateController);
 app.use("/api/update",updateProfileController);
 async function startServer() {
